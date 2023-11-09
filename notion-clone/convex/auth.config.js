7f2eaa467df41f3aa73ea5aa7e71ef02cf16aa00
mirza-z <zaimovic330@gmail.com>
2023-11-09 20:01:52 +0100

    Authentication and databae

---
+++ notion-clone/convex/auth.config.js
@@ -0,0 +1,10 @@
+// https://winning-tahr-22.clerk.accounts.dev
+
+export default {
+    providers: [
+        {
+            domain: "https://winning-tahr-22.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
